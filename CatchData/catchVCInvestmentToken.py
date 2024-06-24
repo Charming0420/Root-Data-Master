02e9244e1e99fc80dda4ee8d17ed7e8c5d4ddb21
余昌旻 Yu Chang-Min <tommy20326999@gmail.com>
2024-06-25 01:19:06 +0800

    改回有頭模式

--- CatchData/catchVCInvestmentToken.py
+++ CatchData/catchVCInvestmentToken.py
@@ -16,14 +16,7 @@ def setup_driver():
     chrome_options.add_argument("--lang=en-US")
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-    
-    # 添加无头模式选项
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-infobars")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    
+
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
     return driver
 
